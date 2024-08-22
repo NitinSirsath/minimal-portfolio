5bfdae7717dd538dea39828b978838f09535c7ef
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2024-08-23 01:22:45 +0530

    UI desing

--- src/routes/pages/home/Index.tsx
+++ src/routes/pages/home/Index.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { ArrowForwardRounded } from '@mui/icons-material';
+import { Button, Typography } from '@mui/material';
 // import globalStyles from '../../../style/globalStyle.module.css';
 
 const HomePage = () => {
@@ -10,6 +11,16 @@ const HomePage = () => {
           Hallo, <span className="text-amber-300">I'm Nitin</span>.
         </Typography>
         <Typography variant="caption">A fullstack-engineer from the pune based in Aurangabad.</Typography>
+        <pre className="py-4">
+          I work with leading-edge companies to create exceptional products. <br />I most recently worked with MoonPay to
+          help make web3 accessible.
+          <br />
+          Before that, I built personal and business banking <br />
+          products for millions of customers at Monzo.
+        </pre>
+        <Button endIcon={<ArrowForwardRounded />} variant="contained">
+          More Infomation
+        </Button>
       </div>
     </div>
   );
