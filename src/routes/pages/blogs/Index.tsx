c010af9bae97c3f553507dd8112f98b55c6bb682
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-01-28 17:15:29 +0530

    added animation in blog page

--- src/routes/pages/blogs/Index.tsx
+++ src/routes/pages/blogs/Index.tsx
@@ -1,18 +1,57 @@
 import { Typography } from '@mui/material';
 import BlogCard from '../../../components/cards/BlogCard';
+import CenterWrapper from '../../../components/wrappers/CenterWrapper';
+import { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 
 const BlogPage = () => {
+  const ref = useRef(null); // Create a ref to observe visibility
+  const isInView = useInView(ref, { once: true, margin: '-50px' }); // Trigger animation when the section enters the viewport
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { staggerChildren: 0.2, duration: 0.8 },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  };
   return (
-    <div>
-      <Typography variant="h3" sx={{ my: 2 }}>
-        Writings
-      </Typography>
-      <div>
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-      </div>
+    <div ref={ref}>
+      <motion.div
+        initial="hidden"
+        animate={isInView ? 'visible' : 'hidden'} // Trigger animation based on visibility
+        variants={containerVariants}
+      >
+        <CenterWrapper widthPercentage={70}>
+          <motion.div variants={itemVariants}>
+            <Typography variant="h3" sx={{ my: 2 }}>
+              Writings
+            </Typography>
+          </motion.div>
+        </CenterWrapper>
+        <CenterWrapper>
+          <motion.div variants={containerVariants}>
+            <motion.div variants={itemVariants}>
+              <BlogCard />
+            </motion.div>
+            <motion.div variants={itemVariants}>
+              <BlogCard />
+            </motion.div>
+            <motion.div variants={itemVariants}>
+              <BlogCard />
+            </motion.div>
+            <motion.div variants={itemVariants}>
+              <BlogCard />
+            </motion.div>
+          </motion.div>
+        </CenterWrapper>
+      </motion.div>
     </div>
   );
 };
